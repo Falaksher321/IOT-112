23a590aca477583b5409b45f736104320cce64b3
M Falak sher <148858533+Falaksher321@users.noreply.github.com>
2023-10-29 09:14:28 -0700

    Add files via upload

---
+++ dimensions.py
@@ -0,0 +1,12 @@
+dimensions = (200,50)
+print(dimensions[0])
+print(dimensions[1])
+# iterate using for loop
+print("original dimesions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400,200)
+print("\n modified dimensions:")
+for dimension in dimensions:
+    print(dimension)
