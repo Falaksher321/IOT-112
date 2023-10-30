2c35179159ead18c001efa5e8863fcc5e4502b4f
M Falak sher <148858533+Falaksher321@users.noreply.github.com>
2023-10-30 04:21:20 -0700

    Add files via upload

---
+++ odd.py
@@ -0,0 +1,9 @@
+# input a number 
+number = int(input("enter a number"))
+# check if the number is even or odd
+if number %2 == 0:
+    # if it is even display "number is even"
+    print("number is even")
+# if it is nor even either even display "number is odd"
+else:
+    print("number is odd")
